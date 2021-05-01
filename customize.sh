839e7c96718d795bafedfefbaecdbbe93ca53e64
Howard20181 <40033067+Howard20181@users.noreply.github.com>
2021-05-02 04:17:22 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -2,5 +2,5 @@ ui_print "- Magisk version: $MAGISK_VER_CODE"
 if [ "$MAGISK_VER_CODE" -lt 21000 ]; then
     ui_print "*********************************************************"
     ui_print "! Please install Magisk v21+"
-    abortC "*********************************************************"
+    abort "*********************************************************"
 fi
